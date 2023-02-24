0ca7fa646f01d64635a7f4936fbe89dd9fdc336b
Hibaq Warsame <hibaq@ualberta.ca>
2023-02-24 03:43:30 -0700

    created interviewerlist file, dynamically added interview items

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,16 @@
 import React from "react";
 import classNames from "classnames";
+import "components/InterviewListItem.scss"
+
 export default function InterviewListItem (props){
-  let interviewerClassName = classNames('interviewers__item', {'--selected': props.selected} );
-  return (<li onClick={()=> props.setInterviewer(props.id)} className={interviewerClassName}>
+  let interviewerClassName = classNames({'interviewers__item': !(props.selected), 'interviewers__item--selected': props.selected});
+  return (<li onClick={()=> props.setInterviewer(props.id)} className={interviewerClassName} selected={props.selected} >
   <img
     className="interviewers__item-image"
     src={props.avatar}
     alt={props.name}
   />
-  Sylvia Palmer
+  {props.selected && <h3>{props.name}</h3>}
 </li>);
 
 }
