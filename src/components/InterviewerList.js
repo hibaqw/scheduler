7a6eef26a4ca0af3f9ef18fca0345d71cc7d76ac
Hibaq Warsame <hibaq@ualberta.ca>
2023-02-26 22:20:25 -0700

    added header and empty files and included them in storyboard

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,11 +6,11 @@ function InterviewerList(props) {
   const InterviewerItem = props.interviewers.map((interviewer) => {
     return (
     <InterviewerListItem
-    key = {interviewer.id}
+    key={interviewer.id}
     name={interviewer.name}
     avatar={interviewer.avatar}
-    selected={interviewer.id === props.interviewer}
-    setInterviewer={() => props.setInterviewer(interviewer.id)}
+    selected={interviewer.id === props.value}
+    setInterviewer={() => props.onChange(interviewer.id)}    
     
     />
     )
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -61,13 +63,13 @@ storiesOf("Button", module)
       backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
     })
     .add("Monday", () => (
-      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+      <DayList days={days} value={"Monday"} onChange={action("setDay")} />
     ))
     .add("Tuesday", () => (
-      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+      <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
     ))
     .add("Wednesday", () => (
-        <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+        <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
     ));
 
     const interviewer = {
@@ -100,7 +102,7 @@ storiesOf("Button", module)
           id={interviewer.id}
           name={interviewer.name}
           avatar={interviewer.avatar}
-          setInterviewer={action("setInterviewer")}
+          setInterviewer={action("setInterviewer")(interviewer.id)}
         />
       ));
 
@@ -113,29 +115,31 @@ storiesOf("Button", module)
       ];
       
       storiesOf("InterviewerList", module)
-        .addParameters({
-          backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-        })
-        .add("Initial", () => (
-          <InterviewerList
-            interviewers={interviewers}
-          />
-        ))
-        .add("Selected", () => (
-          <InterviewerList
-            interviewers={interviewers}
-            interviewer={3}
-          />
-        ))
-        .add("Clickable", () => (
-          <InterviewerList
-            interviewers={interviewers}
-            setInterviewer={action("setInterviewer")}
-          />
-        ));
+      .addParameters({
+        backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+      })
+      .add("Initial", () => (
+        <InterviewerList
+          interviewers={interviewers}
+        />
+      ))
+      .add("Selected", () => (
+        <InterviewerList
+          interviewers={interviewers}
+          value={3}
+        />
+      ))
+      .add("Clickable", () => (
+        <InterviewerList
+          interviewers={interviewers}
+          onChange={action("setInterviewer")}
+        />
+      )); 
 storiesOf("Appointment", module)
 .addParameters({
 backgrounds: [{ name: "white", value: "#fff", default: true }]
  })
 .add("Appointment", () => <Appointment />)
-.add("Appointment with Time", () => <Appointment time="12pm" />);
+.add("Appointment with Time", () => <Appointment time="12pm" />)
+.add("Header", () => <Header time= "12pm"/>)
+.add("Empty", ()=> <Empty onAdd={action("onAdd")}/>)
