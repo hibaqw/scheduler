b2f27699e452f61ea680e811747a8241221fdf91
Hibaq Warsame <hibaq@ualberta.ca>
2023-02-27 09:21:24 -0700

    created logic to extract appointment data using get request

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,48 +4,9 @@ import "components/Application.scss";
 import { useState } from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import Axios from "axios";
+import axios from "axios";
 import { useEffect } from "react";
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -55,17 +16,17 @@ export default function Application(props) {
     appointments: {}
   });
   const setDay = day => setState({ ...state, day });
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
-    //axios request here...
-    Axios.get("/api/days").then((response) => {
-      setDays(response.data);
-    });
-
-  }, [])
-  const setDays = (days) => {
-    setState((prev) => ({...prev, days}));
-}
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments')
+    ]).then((all) => {
+      // set your states here with the correct values...
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data} ))
+    })
+    }, []);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -89,7 +50,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((appointment) => {
+        {dailyAppointments.map((appointment) => {
           return <Appointment
             key={appointment.id}
             {...appointment}
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,8 +3,10 @@ export function getAppointmentsForDay(state, name) {
   if (filteredObj.length === 0) {
     return [];
   }
-  return filteredObj[0].appointments;
-
-
-  
+  // return filteredObj[0].appointments;
+  const getAppointments = filteredObj[0].appointments.map((appointment) => {
+    console.log(state.appointments[appointment]);
+    return state.appointments[appointment];
+  })
+  return getAppointments;
 }
