1f7bc6c4a238190415d53ea0d23e71aaa9901ad1
Hibaq Warsame <hibaq@ualberta.ca>
2023-02-27 00:57:44 -0700

    edited form.js and added create and edit appointment stories to index.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,8 +23,8 @@ export default function Form() {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger>Cancel</Button>
+          <Button confirm>Save</Button>
         </section>
       </section>
     </main>
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -151,3 +152,6 @@ backgrounds: [{ name: "white", value: "#fff", default: true }]
 .add("Confirm", () => <Confirm message ="Delete the appointment?" onConfirm = {action("onConfirm")} onCancel= {action("onCancel")}/>)
 .add("Status", () => <Status message = "Deleting"/>)
 .add("Error", () => <Error message ="Could not delete appointment." onClose = {action("onClose")}/>)
+.add("Create", () => <Form interviewers = {interviewers} onSave = {action("onSave")} onCancel = {action("onCancel")}/>)
+.add("Edit", () => <Form student = "Hibaq Warsame" interviewer = "1" interviewers = {interviewers} onSave = {action("onSave")} onCancel = {action("onCancel")}/>)
+
